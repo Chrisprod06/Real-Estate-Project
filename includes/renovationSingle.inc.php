efda234312d0c63a8bc74f43cf1d23048a8122ed
Christos-345 <77693529+Christos-345@users.noreply.github.com>
2021-03-17 18:17:29 +0200

    Update2

---
+++ includes/renovationSingle.inc.php
@@ -0,0 +1,37 @@
+<?php
+//session_start();
+require_once 'includes/dbh.inc.php';
+
+echo 'THIS IS THE ID OF THE PROPERTY FROM THE URL: ' .$_GET['id']. ''; 
+
+$id =   $_GET['id'];
+$getQuery = "SELECT * from properties WHERE propertyID=2;  ";
+$setQuery = mysqli_query($conn, $getQuery);
+
+$search = array();
+
+while ($row = mysqli_fetch_assoc($setQuery)) 
+{
+
+  
+  'propID'=> $row['propertyID'],
+  'city' => $row['town'],
+  'addr' => $row['address'],
+  'categ' => $row['category'],
+  'totPrice' => $row['totalPrice'],
+  'area' => $row['squarem'],
+  'baths' => $row['bathrooms'],
+  'beds' => $row['bedrooms'],
+  'furnished' => $row['furniture']
+  ); 
+}  
+
+
+foreach ($search as $row){
+  echo '<br>THIS IS THE ID: ' .$row['propID']. '<br>';
+  echo '<br>THIS IS THE category: ' .$row['categ']. '<br>';
+}
+    
+
+
+?>
--- renovationSingle.php
+++ renovationSingle.php
@@ -45,39 +45,7 @@
   </section>
   <!--/ Intro Single End /-->
 
- <?php echo 'THIS IS THE ID OF THE PROPERTY FROM THE URL: ' .$_GET['id']. ''; 
- 
-  $id =   $_GET['id'];
-  require_once 'includes/dbh.inc.php';
-
-  $getQuery = "SELECT * from properties WHERE propertyID=2;  ";
-  $setQuery = mysqli_query($conn, $getQuery);
-
-  $search = array();
- 
-  while ($row = mysqli_fetch_assoc($setQuery)) 
-  {
-
-    $search[] = array(
-    
-    'propID'=> $row['propertyID'],
-    'city' => $row['town'],
-    'addr' => $row['address'],
-    'categ' => $row['category'],
-    'totPrice' => $row['totalPrice'],
-    'area' => $row['squarem'],
-    'baths' => $row['bathrooms'],
-    'beds' => $row['bedrooms'],
-    'furnished' => $row['furniture']
-    );
-
-    foreach ($search as $row){
-      echo '<br>THIS IS THE ID: ' .$row['propID']. '<br>';
-      echo '<br>THIS IS THE category: ' .$row['categ']. '<br>';
-    }
-      
-  } 
- 
+ <?php include_once "includes/renovationSingle.inc.php";
   ?>
 
 
