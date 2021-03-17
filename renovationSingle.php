d0cfc1b5921bee374fbe646ba2abac3d1f1fd5fa
Christos-345 <77693529+Christos-345@users.noreply.github.com>
2021-03-17 17:36:56 +0200

    testing

--- renovationSingle.php
+++ renovationSingle.php
@@ -45,327 +45,41 @@
   </section>
   <!--/ Intro Single End /-->
 
- <!-- Carousel wrapper -->
-<div
-  id="carouselBasicExample"
-  class="carousel slide carousel-fade"
-  data-mdb-ride="carousel"
->
-  <!-- Indicators -->
-  <div class="carousel-indicators">
-    <button
-      type="button"
-      data-mdb-target="#carouselBasicExample"
-      data-mdb-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      type="button"
-      data-mdb-target="#carouselBasicExample"
-      data-mdb-slide-to="1"
-      aria-label="Slide 2"
-    ></button>
-    <button
-      type="button"
-      data-mdb-target="#carouselBasicExample"
-      data-mdb-slide-to="2"
-      aria-label="Slide 3"
-    ></button>
-  </div>
+ <?php echo 'THIS IS THE ID OF THE PROPERTY: ' .$_GET['id']. ''; 
+ 
+  $id =    $_GET['id'];
+  require_once 'includes/dbh.inc.php';
 
-  <!-- Inner -->
-  <div class="carousel-inner">
-    <!-- Single item -->
-    <div class="carousel-item active">
-      <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-        class="d-block w-100"
-        alt="..."
-      />
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </div>
-    </div>
+  $getQuery = "SELECT * from properties WHERE propertyID=2;  ";
+  $setQuery = mysqli_query($conn, $getQuery);
 
-    <!-- Single item -->
-    <div class="carousel-item">
-      <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-        class="d-block w-100"
-        alt="..."
-      />
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-    </div>
+  $search = array();
+ 
+  while ($row = mysqli_fetch_assoc($setQuery)) 
+  {
 
-    <!-- Single item -->
-    <div class="carousel-item">
-      <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-        class="d-block w-100"
-        alt="..."
-      />
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </div>
-    </div>
-  </div>
-  <!-- Inner -->
+    $search[] = array(
+    
+    'propID'=> $row['propertyID'],
+    'city' => $row['town'],
+    'addr' => $row['address'],
+    'categ' => $row['category'],
+    'totPrice' => $row['totalPrice'],
+    'area' => $row['squarem'],
+    'baths' => $row['bathrooms'],
+    'beds' => $row['bedrooms'],
+    'furnished' => $row['furniture']
+    );
 
-  <!-- Controls -->
-  <button
-    class="carousel-control-prev"
-    type="button"
-    data-mdb-target="#carouselBasicExample"
-    data-mdb-slide="prev"
-  >
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next"
-    type="button"
-    data-mdb-target="#carouselBasicExample"
-    data-mdb-slide="next"
-  >
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<!-- Carousel wrapper -->
-          
-          <div class="row justify-content-between">
-            <div class="col-md-5 col-lg-4">
-              
-              <div class="property-summary">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="title-box-d section-t4">
-                      <h3 class="title-d">Quick Summary</h3>
-                    </div>
-                  </div>
-                </div>
-                <div class="summary-list">
-                  <ul class="list">
-                    <li class="d-flex justify-content-between">
-                      <strong>Property ID:</strong>
-                      <span><?php
-                      echo ''.$_GET['id'].''; ?></span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Location:</strong>
-                      <span>Limassol, Odos Ellados, 14</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Property Type:</strong>
-                      <span>Appartment</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Status:</strong>
-                      <span>Sale</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Area:</strong>
-                      <span>340m
-                        <sup>2</sup>
-                      </span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Beds:</strong>
-                      <span>1</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Baths:</strong>
-                      <span>1</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Garage:</strong>
-                      <span>1</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Renovated:</strong>
-                      <span>Yes</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Furnished</strong>
-                      <span>No</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Date of Construction:</strong>
-                      <span>25 June 1999</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Floors:</strong>
-                      <span>2</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Available From:</strong>
-                      <span>22 November 2021</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Heating System:</strong>
-                      <span>Included</span>
-                    </li>
-                    <li class="d-flex justify-content-between">
-                      <strong>Price per m<sup>2</sup>:</strong>
-                      <span>$1000</span>
-                    </li>
-                  </ul>
-                  <div class="post-share">
-                    <span>Share: </span>
-                    <ul class="list-inline socials">
-                      <li class="list-inline-item">
-                        <a href="#">
-                          <i class="fa fa-facebook" aria-hidden="true"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="#">
-                          <i class="fa fa-twitter" aria-hidden="true"></i>
-                        </a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="#">
-                          <i class="fa fa-instagram" aria-hidden="true"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-7 col-lg-7 section-md-t3">
-              <div class="row">
-                <div class="col-sm-12">
-                  <div class="title-box-d">
-                    <h3 class="title-d">Property Description</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="property-description">
-                <p class="description color-text-a text-justify">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies
+    foreach ($search as $row){
+      echo '<br>THIS IS THE ID: ' .$row['propID']. '<br>';
+      echo '<br>THIS IS THE category: ' .$row['categ']. '<br>';
+    }
+      
+  } 
+ 
+  ?>
 
-                </p>
-              
-              </div>
-              <div class="row section-t3">
-                <div class="col-sm-12">
-                  <div class="title-box-d">
-                    <h3 class="title-d">Amenities</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="amenities-list color-text-a">
-                <ul class="list-a no-margin">
-                  <li>Balcony</li>
-                  <li>Outdoor Kitchen</li>
-                  <li>Cable Tv</li>
-                  <li>King Size Bed</li>
-                  <li>Central Heating</li>
-                  <li>Internet</li>
-                  <li>Parking</li>
-                  <li>Fireplace</li>
-                  <li>Concrete Flooring</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-10 offset-md-1">
-          <ul class="nav nav-pills-a nav-pills mb-3 section-t3" id="pills-tab" role="tablist">
-            <li class="nav-item">
-              <a class="nav-link active" id="pills-video-tab" data-toggle="pill" href="#pills-video" role="tab"
-                aria-controls="pills-video" aria-selected="true">Video</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="pills-plans-tab" data-toggle="pill" href="#pills-plans" role="tab"
-                aria-controls="pills-plans" aria-selected="false">Virtual Tour</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" id="pills-map-tab" data-toggle="pill" href="#pills-map" role="tab"
-                aria-controls="pills-map" aria-selected="false">Location</a>
-            </li>
-          </ul>
-          <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-video" role="tabpanel" aria-labelledby="pills-video-tab">
-              <iframe src="https://player.vimeo.com/video/73221098" width="100%" height="460" frameborder="0"
-                webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-            </div>
-            <div class="tab-pane fade" id="pills-plans" role="tabpanel" aria-labelledby="pills-plans-tab">
-              <img src="img/plan2.jpg" alt="" class="img-fluid">
-            </div>
-            <div class="tab-pane fade" id="pills-map" role="tabpanel" aria-labelledby="pills-map-tab">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1422937950147!2d-73.98731968482413!3d40.75889497932681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes+Square!5e0!3m2!1ses-419!2sve!4v1510329142834"
-                width="100%" height="460" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="row section-t3">
-            <div class="col-sm-12">
-              <div class="title-box-d">
-                <h3 class="title-d">Contact </h3>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6 col-lg-4">
-              <img src="img/logo.png" alt="" class="img-fluid">
-            </div>
-            <div class="col-md-6 col-lg-4">
-              <div class="property-agent">
-                <h4 class="title-agent">The Company</h4>
-                
-                <ul class="list-unstyled">
-                  <li class="d-flex justify-content-between">
-                    <strong>Office:</strong>
-                    <span class="color-text-a">+357 25778899</span>
-                  </li>
-                  <li class="d-flex justify-content-between">
-                    <strong>Email:</strong>
-                    <span class="color-text-a">apm.smarthouses@gmail.com</span>
-                  </li>
-                  <li class="d-flex justify-content-between">
-                    <strong>Skype:</strong>
-                    <span class="color-text-a">live:.cid.d64cd0a5de872755</span>
-                  </li>
-                </ul>
-                <div class="socials-a">
-                  <ul class="list-inline">
-                    <li class="list-inline-item">
-                      <a href="#">
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="#">
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="#">
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--/ Property Single End /-->
 
   <!--Footer-->
   <?php
