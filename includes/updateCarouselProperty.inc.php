c90d2927abbde86aee42c6193cc10a2d4c280e4a
kiriakosapex <77694639+kiriakosapex@users.noreply.github.com>
2021-03-29 21:56:06 +0300

    kakos mult

--- includes/updateCarouselProperty.inc.php
+++ includes/updateCarouselProperty.inc.php
@@ -13,13 +13,13 @@ if($resultCheck > 0){
      <div class='carousel-item-a'> <img src= '{$row['photo1']}' width='100' height='550' alt=''> <div class='text-block'>  </div> </div>
      <div class='carousel-item-b'> <img src= '{$row['photo2']}' width='100' height='550' alt=''> <div class='text-block'>  </div> </div>
      <div class='carousel-item-c'> <img src= '{$row['photo3']}' width='100' height='550' alt=''> <div class='text-block'>  </div> </div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo4']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo5']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo6']}' width='100' height='550' alt=''> <div class='text-block'> </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo7']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo8']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>  
-     <div class='carousel-item-c'> <img src= '{$row['photo9']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>
-     <div class='carousel-item-c'> <img src= '{$row['photo10']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>   
+     <div class='carousel-item-d'> <img src= '{$row['photo4']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
+     <div class='carousel-item-e'> <img src= '{$row['photo5']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
+     <div class='carousel-item-f'> <img src= '{$row['photo6']}' width='100' height='550' alt=''> <div class='text-block'> </div></div> 
+     <div class='carousel-item-g'> <img src= '{$row['photo7']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div> 
+     <div class='carousel-item-h'> <img src= '{$row['photo8']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>  
+     <div class='carousel-item-i'> <img src= '{$row['photo9']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>
+     <div class='carousel-item-j'> <img src= '{$row['photo10']}' width='100' height='550' alt=''> <div class='text-block'>  </div></div>   
          
      ";
      
--- includes/updateCarouselRen.inc.php
+++ includes/updateCarouselRen.inc.php
@@ -2,7 +2,7 @@
 
 include_once "dbh.inc.php";
 $id = $_GET['id'];
-$sql = "SELECT * FROM multimedia WHERE propertyID = $id;";
+$sql = "SELECT * FROM multimediarenovations WHERE propertyID = $id;";
 $result = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
 
@@ -13,11 +13,11 @@ if($resultCheck > 0){
      <div class='carousel-item-a'> <img src= '{$row['photo1']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div> </div>
      <div class='carousel-item-b'> <img src= '{$row['photo2']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div> </div>
      <div class='carousel-item-c'> <img src= '{$row['photo3']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div> </div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo4']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo5']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo6']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo7']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div></div> 
-     <div class='carousel-item-c'> <img src= '{$row['photo8']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div>     
+     <div class='carousel-item-d'> <img src= '{$row['photo4']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div> 
+     <div class='carousel-item-e'> <img src= '{$row['photo5']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div></div> 
+     <div class='carousel-item-f'> <img src= '{$row['photo6']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div> 
+     <div class='carousel-item-g'> <img src= '{$row['photo7']}' width='100' height='550' alt=''> <div class='text-block'> <h>Before</h> </div></div> 
+     <div class='carousel-item-h'> <img src= '{$row['photo8']}' width='100' height='550' alt=''> <div class='text-block'> <h>After</h> </div></div>     
          
      ";
      
