a2e2a342c784e7157970ac20ec8e6d96131990ae
Christophoros Prodromou <52533416+Chrisprod06@users.noreply.github.com>
2021-04-15 11:09:46 +0300

    Update unsubscribeNewsletter.php

--- unsubscribeNewsletter.php
+++ unsubscribeNewsletter.php
@@ -1,86 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Unsubscribe | APM Smart Houses</title>
-<link href="img/favicon.png" rel="icon">
-<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="css/style.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-<style>
-	.login-form {
-		width: 340px;
-    	margin: 50px auto;
-	}
-    .login-form form {
-    	margin-bottom: 15px;
-        background: #f7f7f7;
-        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-        padding: 30px;
-    }
-    .login-form h2 {
-        margin: 0 0 15px;
-    }
-    .form-control, .btn {
-        min-height: 38px;
-        border-radius: 2px;
-    }
-    .btn {        
-        font-size: 15px;
-        font-weight: bold;
-    }
-</style>
-</head>
-<body>
-
-    <?php
-        session_start();
-        if(!isset($_SESSION['userID'])){
-            echo '<div class="login-form">
-                <form action="includes/login.inc.php" method="POST">
-                    <h2 class="text-center">Unsubscribe from newsletter</h2>       
-                    <p class="text-center">Please login in order to be able to unsubscribe.</p>
-                    <div class="form-group">
-                        <button type="submit" value="sumbit" class="btn btn-primary btn-block">Login</button>
-                    </div>
-                </form>
-            </div>';
-
-        }else{
-            echo'<div class="login-form">
-                <form action="includes/delnews.inc.php" method="POST">
-                    <h2 class="text-center">Unsubscribe from newsletter</h2>       
-                    <p class="text-center">We are sorry to see you go.</p>
-                    <div class="form-group">
-                        <button type="submit" value="sumbit" class="btn btn-primary btn-block">Unsubscribe</button>
-                    </div>
-                </form>
-            </div>';
-
-             
-            } 
-        
-        if(isset($_GET['error'])){    
-            if($_GET['error'] == 'stmtFailed')
-                {
-               echo '<p class = "text-danger text-center " >Something went wrong, try again!</p>';
+<?php
+$title = "Unsubcribe Newsletter | APM Smart Houses";
+include_once 'includes/header.inc.php';
+?>
+<section class="intro-single">
+    <div class="login-form">
+        <form action="includes/resetPasswordRequest.inc.php" method="post">
+            <h2 class="text-center">Reset your password</h2>
+            <p class="text-center">Type your email and we will send you instructions on how to reset your password.</p>
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" placeholder="Email" required="required">
+            </div>
+            <!--Error messages-->
+            <?php
+            if (isset($_GET['error'])) {
+                if ($_GET['error'] == 'stmtFailed') {
+                    echo '<p class = "text-danger text-center " >Something went wrong. Please try again.</p>';
                 }
-        
-            else if ($_GET['error'] == 'none' )
-                {
-                echo '<p class = "text-danger text-center " >Unsubscribed Successfully.</p>';
+            }
+
+            if (isset($_GET['request'])) {
+                if ($_GET['request'] == 'success') {
+                    echo '
+                    <script>
+                    $(document).ready(function(){
+                      Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Your request has been sent! Please check your inbox.",
+                        showConfirmButton: false,
+                        timer: 1500                 
+                      }).then(function() {
+                        window.location.href = "index.php";
+                      })
+                    });                 
+                    </script>
+                    ';
+                } else if ($_GET['request'] == 'fail') {
+                    echo '<p class = "text-danger text-center " >Something went wrong. Please try again.</p>';
                 }
-        
-        
-                        }          
-               ?>
-             
-             
-    
-</div>
-</body>
-</html>                                		
+            }
+            ?>
+
+            <div class="form-group">
+                <button type="submit" name="submitResetPasswordRequest" class="btn btn-a">Send Request</button>
+            </div>
+        </form>
+
+    </div>
+</section>
+
+<?php
+include_once "includes/footer.inc.php";
+?>
